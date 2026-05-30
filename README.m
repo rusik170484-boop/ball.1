aed712c9d275fbad344de14dd36444252c41d730
rusik170484-boop <rusik170484@gmail.com>
2026-05-30 12:49:48 +0300

    Update README.m

--- README.m
+++ README.m
@@ -25,32 +25,46 @@ class Player(GameSprite):
             self.rect.y -= self.speed
 
 window = display.set_mode((700, 500))
-display.set_caption('Пинг понг')
-########################### 
-racketl = Player('racket.png',  175, 30, 5,(30, 150))
-racketr = Player('racket.png',  175, 640, 5,(30, 150))
-###########################
+display.set_caption('Пинг понг') 
 clock = time.Clock()
 FPS = 60
 run = True
 finish = False
-
 font.init()
 my_font = font.SysFont("Arial", 35)
-win = my_font.render("Вы победили!", True, (255, 255, 0))
-lose_lost = my_font.render("Вы пропустили слишком много тарелок!", True, (255, 0, 0))
-lose_walls = my_font.render("Вас сбили!", True, (255, 0, 0))
+
+win_left = my_font.render("Победила левая ракетка", True, (255, 255, 0))
+
+left_r = Player('racket.png', 250, 50, 3, (30, 150))
+right_r = Player('racket.png', 250, 630, 3, (30, 150))
+ball = GameSprite('tenis_ball.png',200, 300, 0, (50, 50))
+speed_x, speed_y = 2, 2
 while run:
+    window.fill((117, 153, 161))
     for e  in event.get():
         if e.type == QUIT:
             run = False
-    window.fill((200, 255, 255))
     if not finish:
-        ###########################
-        racketl.reset()
-        racketr.reset()
-        racketl.update_l()
-        racketr.update_r()
-        ###########################
+        ball.rect.x += speed_x
+        ball.rect.y += speed_y
+
+        left_r.reset()
+        right_r.reset()
+
+        ball.reset()
+
+        left_r.update_l()
+        right_r.update_r()
+
+        if ball.rect.y < 0 or ball.rect.y > 450: 
+            speed_y *= -1
+        if ball.rect.colliderect(left_r) or ball.rect.colliderect(right_r):
+            speed_x *= -1
+
+    if ball.rect.x > 700:
+        finish = True
+        window.blit(win_left, (200, 230))
+
     display.update() 
     clock.tick(60)
+
